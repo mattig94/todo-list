951fb2be4e48b78b374acdbf9c2661cf067183ad
mattig94 <mattig94@users.noreply.github.com>
2019-12-17 12:08:27 -0500

    add, complete, delete, and edit todos

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
     <ul class="todos">
       <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }}
+        <input type="checkbox" v-model="todo.completed">
+        <span @dblclick="editTodo(todo)" v-if="!todo.edit">
+          {{ todo.label }}
+        </span>
+        <input v-else type="text" v-model="todo.label" @keydown.enter="doneEditing(todo)">
+        <button @click="removeTodo(todo)">Delete</button>
       </li>
     </ul>
   </div>
@@ -19,12 +24,25 @@ export default {
   },
   methods: {
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false, edit: false});
       this.currentTodo='';
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.edit = true;
+    },
+    doneEditing(todo) {
+      todo.edit = false;
     }
   }
 };
 </script>
 
 <style>
+li {
+  list-style: none;
+}
 </style>
